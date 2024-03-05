bd25270d81652a7fee81a7da0505b45348dd6194
juan <jc2020yate@outlook.com>
2024-03-05 14:56:35 -0500

    Crear-un-array-con-elementos-de-diferentes-tipos-de-datos

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ let arrayVacio = [];
 
 //Crear un array con elementos
 let arrayConElementos = [1, 'dos', true, { nombre: 'Juan' }];
+
+//Crear un array con elementos de diferentes tipos de datos
+let arrayTiposDiferentes = [1, 'dos', true, { nombre: 'Juan' }];
